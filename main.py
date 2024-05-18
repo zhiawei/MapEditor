c14b0c9bf9a2fb35efc998d51779ca45d8ef5b0b
zhiawei <zhiawei@gmail.com>
2024-05-18 18:05:45 +0100

    added grid

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame
+import sys
 
-(width, height) = (600, 400)
-background_colour = (0, 0, 0)
+#Color
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-screen.fill(background_colour)
+resolution = (WIDTH, HEIGHT) = (600, 400)
+background_colour = (BLACK)
+clock = pygame.time.Clock()
+
+pygame.init()
+SCREEN = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+SCREEN.fill(background_colour)
 pygame.display.set_caption('MapEditor')
 
 pygame.display.flip()
 
-running = True
-while running:
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, WIDTH, blockSize):
+        for y in range(0, HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+while True:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    screen.fill(background_colour)
-    pygame.display.update()
+    SCREEN.fill(background_colour)
+    drawGrid()
+    pygame.display.flip()
+
