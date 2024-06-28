5fd36f3a9d28bb62a50e5981254bb5be510119ee
Low Zhia Wei <zhiawei@gmail.com>
2024-06-28 11:29:32 +0100

    add: GridView

--- main/lib/utilities/custom.dart
+++ main/lib/utilities/custom.dart
@@ -27,15 +27,61 @@ class TileView_container extends StatelessWidget {
   }
 }
 
-class GridView_container extends StatelessWidget {
+class GridView_container extends StatefulWidget {
+  @override
+  State<GridView_container> createState() => _GridView_container();
+}
+
+class _GridView_container extends State<GridView_container> {
+  Offset _offset = Offset.zero;
+  Offset _startPosition = Offset.zero;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: custonmDeco(),
-      child: Center(
-        child: Text('Grid View', style: TextStyle(color: Colors.white)),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: GestureDetector(
+            onPanStart: (details) {
+              setState(() {
+                _startPosition = details.localPosition;
+              });
+            },
+            onPanUpdate: (details) {
+              setState(() {
+                _offset += details.localPosition - _startPosition;
+                _startPosition = details.localPosition;
+              });
+            },
+            child: CustomPaint(
+                painter: GridPainter(_offset), child: Container())));
+  }
+}
+
+class GridPainter extends CustomPainter {
+  final Offset offset;
+
+  GridPainter(this.offset);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = const Color.fromARGB(82, 94, 165, 115)
+      ..strokeWidth = 1;
+
+    double cellSize = 30;
+
+    for (double x = offset.dx % cellSize; x < size.width; x += cellSize) {
+      canvas.drawLine(Offset(x, 0), Offset(x, size.height), paint);
+    }
+
+    for (double y = offset.dy % cellSize; y < size.height; y += cellSize) {
+      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
